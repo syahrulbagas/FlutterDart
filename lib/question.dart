361db30ded95b673a008af8aa5cecf2e17189b2a
Syahrul Bagas <71810571+syahrulbagas@users.noreply.github.com>
2022-11-08 22:45:29 +0700

    Delete lib directory

--- lib/question.dart
+++
@@ -1,7 +0,0 @@
-class Question {
-  String questionText;
-  bool questionAnswer;
-
-  // Question({required this.questionText, required this.questionAnswer});
-  Question(this.questionText, this.questionAnswer);
-}
